8194df222c9db64ef72f9f339faa542eb4ac60d3
h1soft <h@h1soft.net>
2016-05-30 22:12:06 +0800

    update

--- UeditorAction.php
+++ UeditorAction.php
@@ -6,263 +6,254 @@ use Yii;
 use yii\base\Action;
 use yii\helpers\ArrayHelper;
 
-class UeditorAction extends Action {
-	/**
-	 *
-	 * @var array
-	 */
-	public $config = [ ];
-	public function init() {
-		Yii::$app->request->enableCsrfValidation = false;
-		$_config = require (__DIR__ . '/config.php');
-		$this->config = ArrayHelper::merge ( $_config, $this->config );
-		parent::init ();
-	}
-	public function run() {
-		$this->handleAction ();
-	}
-	
-	/**
-	 * ´¦Àíaction
-	 */
-	protected function handleAction() {
-		$action = Yii::$app->request->get ( 'action' );
-		switch ($action) {
-			case 'config' :
-				$result = json_encode ( $this->config );
-				break;
-			
-			/* ÉÏ´«Í¼Æ¬ */
-			case 'uploadimage' :
-			/* ÉÏ´«Í¿Ñ» */
-			case 'uploadscrawl' :
-			/* ÉÏ´«ÊÓÆµ */
-			case 'uploadvideo' :
-			/* ÉÏ´«ÎÄ¼þ */
-			case 'uploadfile' :
-				$result = $this->actionUpload ();
-				break;
-			
-			/* ÁÐ³öÍ¼Æ¬ */
-			case 'listimage' :
-			/* ÁÐ³öÎÄ¼þ */
-			case 'listfile' :
-				$result = $this->actionList ();
-				break;
-			
-			/* ×¥È¡Ô¶³ÌÎÄ¼þ */
-			case 'catchimage' :
-				$result = $this->actionCrawler ();
-				break;
-			
-			default :
-				$result = json_encode ( array (
-						'state' => 'ÇëÇóµØÖ·³ö´í' 
-				) );
-				break;
-		}
-		/* Êä³ö½á¹û */
-		if (isset ( $_GET ["callback"] )) {
-			if (preg_match ( "/^[\w_]+$/", $_GET ["callback"] )) {
-				echo htmlspecialchars ( $_GET ["callback"] ) . '(' . $result . ')';
-			} else {
-				echo json_encode ( array (
-						'state' => 'callback params invalid' 
-				) );
-			}
-		} else {
-			echo $result;
-		}
-	}
-	
-	/**
-	 * ÉÏ´«
-	 *
-	 * @return string
-	 */
-	protected function actionUpload() {
-		$base64 = "upload";
-		switch (htmlspecialchars ( $_GET ['action'] )) {
-			case 'uploadimage' :
-				$config = array (
-						"pathRoot" => ArrayHelper::getValue ( $this->config, "imageRoot", $_SERVER ['DOCUMENT_ROOT'] ),
-						"pathFormat" => $this->config ['imagePathFormat'],
-						"maxSize" => $this->config ['imageMaxSize'],
-						"allowFiles" => $this->config ['imageAllowFiles'] 
-				);
-				$fieldName = $this->config ['imageFieldName'];
-				break;
-			case 'uploadscrawl' :
-				$config = array (
-						"pathRoot" => ArrayHelper::getValue ( $this->config, "scrawlRoot", $_SERVER ['DOCUMENT_ROOT'] ),
-						"pathFormat" => $this->config ['scrawlPathFormat'],
-						"maxSize" => $this->config ['scrawlMaxSize'],
-						"allowFiles" => $this->config ['scrawlAllowFiles'],
-						"oriName" => "scrawl.png" 
-				);
-				$fieldName = $this->config ['scrawlFieldName'];
-				$base64 = "base64";
-				break;
-			case 'uploadvideo' :
-				$config = array (
-						"pathRoot" => ArrayHelper::getValue ( $this->config, "videoRoot", $_SERVER ['DOCUMENT_ROOT'] ),
-						"pathFormat" => $this->config ['videoPathFormat'],
-						"maxSize" => $this->config ['videoMaxSize'],
-						"allowFiles" => $this->config ['videoAllowFiles'] 
-				);
-				$fieldName = $this->config ['videoFieldName'];
-				break;
-			case 'uploadfile' :
-			default :
-				$config = array (
-						"pathRoot" => ArrayHelper::getValue ( $this->config, "fileRoot", $_SERVER ['DOCUMENT_ROOT'] ),
-						"pathFormat" => $this->config ['filePathFormat'],
-						"maxSize" => $this->config ['fileMaxSize'],
-						"allowFiles" => $this->config ['fileAllowFiles'] 
-				);
-				$fieldName = $this->config ['fileFieldName'];
-				break;
-		}
-		
-		$up = new Uploader( $fieldName, $config, $base64 );
-		
-		return json_encode ( $up->getFileInfo () );
-	}
-	
-	/**
-	 * »ñÈ¡ÒÑÉÏ´«µÄÎÄ¼þÁÐ±í
-	 *
-	 * @return string
-	 */
-	protected function actionList() {
-		/* ÅÐ¶ÏÀàÐÍ */
-		switch ($_GET ['action']) {
-			/* ÁÐ³öÎÄ¼þ */
-			case 'listfile' :
-				$allowFiles = $this->config ['fileManagerAllowFiles'];
-				$listSize = $this->config ['fileManagerListSize'];
-				$path = $this->config ['fileManagerListPath'];
-				break;
-			/* ÁÐ³öÍ¼Æ¬ */
-			case 'listimage' :
-			default :
-				$allowFiles = $this->config ['imageManagerAllowFiles'];
-				$listSize = $this->config ['imageManagerListSize'];
-				$path = $this->config ['imageManagerListPath'];
-		}
-		$allowFiles = substr ( str_replace ( ".", "|", join ( "", $allowFiles ) ), 1 );
-		
-		/* »ñÈ¡²ÎÊý */
-		$size = isset ( $_GET ['size'] ) ? htmlspecialchars ( $_GET ['size'] ) : $listSize;
-		$start = isset ( $_GET ['start'] ) ? htmlspecialchars ( $_GET ['start'] ) : 0;
-		$end = ( int ) $start + ( int ) $size;
-		
-		/* »ñÈ¡ÎÄ¼þÁÐ±í */
-		$path = $_SERVER ['DOCUMENT_ROOT'] . (substr ( $path, 0, 1 ) == "/" ? "" : "/") . $path;
-		$files = $this->getfiles ( $path, $allowFiles );
-		if (! count ( $files )) {
-			return json_encode ( array (
-					"state" => "no match file",
-					"list" => array (),
-					"start" => $start,
-					"total" => count ( $files ) 
-			) );
-		}
-		
-		/* »ñÈ¡Ö¸¶¨·¶Î§µÄÁÐ±í */
-		$len = count ( $files );
-		for($i = min ( $end, $len ) - 1, $list = array (); $i < $len && $i >= 0 && $i >= $start; $i --) {
-			$list [] = $files [$i];
-		}
-		// µ¹Ðò
-		// for ($i = $end, $list = array(); $i < $len && $i < $end; $i++){
-		// $list[] = $files[$i];
-		// }
-		
-		/* ·µ»ØÊý¾Ý */
-		$result = json_encode ( array (
-				"state" => "SUCCESS",
-				"list" => $list,
-				"start" => $start,
-				"total" => count ( $files ) 
-		) );
-		
-		return $result;
-	}
-	
-	/**
-	 * ×¥È¡Ô¶³ÌÍ¼Æ¬
-	 *
-	 * @return string
-	 */
-	protected function actionCrawler() {
-		/* ÉÏ´«ÅäÖÃ */
-		$config = array (
-				"pathFormat" => $this->config ['catcherPathFormat'],
-				"maxSize" => $this->config ['catcherMaxSize'],
-				"allowFiles" => $this->config ['catcherAllowFiles'],
-				"oriName" => "remote.png" 
-		);
-		$fieldName = $this->config ['catcherFieldName'];
-		
-		/* ×¥È¡Ô¶³ÌÍ¼Æ¬ */
-		$list = array ();
-		if (isset ( $_POST [$fieldName] )) {
-			$source = $_POST [$fieldName];
-		} else {
-			$source = $_GET [$fieldName];
-		}
-		foreach ( $source as $imgUrl ) {
-			$item = new Uploader ( $imgUrl, $config, "remote" );
-			$info = $item->getFileInfo ();
-			array_push ( $list, array (
-					"state" => $info ["state"],
-					"url" => $info ["url"],
-					"size" => $info ["size"],
-					"title" => htmlspecialchars ( $info ["title"] ),
-					"original" => htmlspecialchars ( $info ["original"] ),
-					"source" => htmlspecialchars ( $imgUrl ) 
-			) );
-		}
-		
-		/* ·µ»Ø×¥È¡Êý¾Ý */
-		return json_encode ( array (
-				'state' => count ( $list ) ? 'SUCCESS' : 'ERROR',
-				'list' => $list 
-		) );
-	}
-	
-	/**
-	 * ±éÀú»ñÈ¡Ä¿Â¼ÏÂµÄÖ¸¶¨ÀàÐÍµÄÎÄ¼þ
-	 *
-	 * @param
-	 *        	$path
-	 * @param
-	 *        	$allowFiles
-	 * @param array $files        	
-	 * @return array|null
-	 */
-	protected function getfiles($path, $allowFiles, &$files = array()) {
-		if (! is_dir ( $path ))
-			return null;
-		if (substr ( $path, strlen ( $path ) - 1 ) != '/')
-			$path .= '/';
-		$handle = opendir ( $path );
-		while ( false !== ($file = readdir ( $handle )) ) {
-			if ($file != '.' && $file != '..') {
-				$path2 = $path . $file;
-				if (is_dir ( $path2 )) {
-					$this->getfiles ( $path2, $allowFiles, $files );
-				} else {
-					if (preg_match ( "/\.(" . $allowFiles . ")$/i", $file )) {
-						$files [] = array (
-								'url' => substr ( $path2, strlen ( $_SERVER ['DOCUMENT_ROOT'] ) ),
-								'mtime' => filemtime ( $path2 ) 
-						);
-					}
-				}
-			}
-		}
-		return $files;
-	}
+class UeditorAction extends Action
+{
+    /**
+     *
+     * @var array
+     */
+    public $config = [];
+
+    public function init()
+    {
+        parent::init();
+        Yii::$app->request->enableCsrfValidation = false;
+        $_config = require(__DIR__ . '/config.php');
+        $this->config = ArrayHelper::merge($_config, $this->config);
+//        print_r($this->config);die;
+    }
+
+    public function run()
+    {
+        return $this->handleAction();
+    }
+
+    protected function handleAction()
+    {
+        $action = Yii::$app->request->get('action');
+        switch ($action) {
+            case 'config':
+                Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+                return $this->config;
+            case 'uploadimage' :
+            case 'uploadscrawl' :
+            case 'uploadvideo' :
+            case 'uploadfile' :
+                $result = $this->actionUpload();
+                break;
+
+            case 'listimage' :
+            case 'listfile' :
+                $result = $this->actionList();
+                break;
+
+            case 'catchimage' :
+                $result = $this->actionCrawler();
+                break;
+
+            default :
+                $result = json_encode(array(
+                    'state' => 'è¯·æ±‚åœ°å€å‡ºé”™'
+                ));
+                break;
+        }
+        if (isset ($_GET ["callback"])) {
+            if (preg_match("/^[\w_]+$/", $_GET ["callback"])) {
+                echo htmlspecialchars($_GET ["callback"]) . '(' . $result . ')';
+            } else {
+                echo json_encode(array(
+                    'state' => 'callback params invalid'
+                ));
+            }
+        } else {
+            echo $result;
+        }
+    }
+
+    /**
+     * ï¿½ä¸Šä¼ æ–‡ä»¶
+     *
+     * @return string
+     */
+    protected function actionUpload()
+    {
+        $base64 = "upload";
+        switch (htmlspecialchars($_GET ['action'])) {
+            case 'uploadimage' :
+                $config = array(
+                    "pathRoot" => ArrayHelper::getValue($this->config, "imageRoot", $_SERVER ['DOCUMENT_ROOT']),
+                    "pathFormat" => $this->config ['imagePathFormat'],
+                    "maxSize" => $this->config ['imageMaxSize'],
+                    "allowFiles" => $this->config ['imageAllowFiles']
+                );
+                $fieldName = $this->config ['imageFieldName'];
+                break;
+            case 'uploadscrawl' :
+                $config = array(
+                    "pathRoot" => ArrayHelper::getValue($this->config, "scrawlRoot", $_SERVER ['DOCUMENT_ROOT']),
+                    "pathFormat" => $this->config ['scrawlPathFormat'],
+                    "maxSize" => $this->config ['scrawlMaxSize'],
+                    "allowFiles" => $this->config ['scrawlAllowFiles'],
+                    "oriName" => "scrawl.png"
+                );
+                $fieldName = $this->config ['scrawlFieldName'];
+                $base64 = "base64";
+                break;
+            case 'uploadvideo' :
+                $config = array(
+                    "pathRoot" => ArrayHelper::getValue($this->config, "videoRoot", $_SERVER ['DOCUMENT_ROOT']),
+                    "pathFormat" => $this->config ['videoPathFormat'],
+                    "maxSize" => $this->config ['videoMaxSize'],
+                    "allowFiles" => $this->config ['videoAllowFiles']
+                );
+                $fieldName = $this->config ['videoFieldName'];
+                break;
+            case 'uploadfile' :
+            default :
+                $config = array(
+                    "pathRoot" => ArrayHelper::getValue($this->config, "fileRoot", $_SERVER ['DOCUMENT_ROOT']),
+                    "pathFormat" => $this->config ['filePathFormat'],
+                    "maxSize" => $this->config ['fileMaxSize'],
+                    "allowFiles" => $this->config ['fileAllowFiles']
+                );
+                $fieldName = $this->config ['fileFieldName'];
+                break;
+        }
+        $up = new Uploader($fieldName, $config, $base64);
+
+        return json_encode($up->getFileInfo());
+    }
+
+    /**
+     *
+     * æ–‡ä»¶ç®¡ç†
+     * @return string
+     */
+    protected function actionList()
+    {
+        switch ($_GET ['action']) {
+            case 'listfile' :
+                $allowFiles = $this->config ['fileManagerAllowFiles'];
+                $listSize = $this->config ['fileManagerListSize'];
+                $path = $this->config ['fileManagerListPath'];
+                break;
+            case 'listimage' :
+            default :
+                $allowFiles = $this->config ['imageManagerAllowFiles'];
+                $listSize = $this->config ['imageManagerListSize'];
+                $path = $this->config ['imageManagerListPath'];
+        }
+        $allowFiles = substr(str_replace(".", "|", join("", $allowFiles)), 1);
+
+        $size = isset ($_GET ['size']) ? htmlspecialchars($_GET ['size']) : $listSize;
+        $start = isset ($_GET ['start']) ? htmlspecialchars($_GET ['start']) : 0;
+        $end = ( int )$start + ( int )$size;
+
+        $path = $_SERVER ['DOCUMENT_ROOT'] . (substr($path, 0, 1) == "/" ? "" : "/") . $path;
+        $files = $this->getfiles($path, $allowFiles);
+        if (!count($files)) {
+            return json_encode(array(
+                "state" => "no match file",
+                "list" => array(),
+                "start" => $start,
+                "total" => count($files)
+            ));
+        }
+
+        $len = count($files);
+        for ($i = min($end, $len) - 1, $list = array(); $i < $len && $i >= 0 && $i >= $start; $i--) {
+            $list [] = $files [$i];
+        }
+        // ï¿½ï¿½ï¿½ï¿½
+        // for ($i = $end, $list = array(); $i < $len && $i < $end; $i++){
+        // $list[] = $files[$i];
+        // }
+
+        $result = json_encode(array(
+            "state" => "SUCCESS",
+            "list" => $list,
+            "start" => $start,
+            "total" => count($files)
+        ));
+
+        return $result;
+    }
+
+    /**
+     * èŽ·å–æ–‡ä»¶
+     *
+     * @param
+     *            $path
+     * @param
+     *            $allowFiles
+     * @param array $files
+     * @return array|null
+     */
+    protected function getfiles($path, $allowFiles, &$files = array())
+    {
+        if (!is_dir($path))
+            return null;
+        if (substr($path, strlen($path) - 1) != '/')
+            $path .= '/';
+        $handle = opendir($path);
+        while (false !== ($file = readdir($handle))) {
+            if ($file != '.' && $file != '..') {
+                $path2 = $path . $file;
+                if (is_dir($path2)) {
+                    $this->getfiles($path2, $allowFiles, $files);
+                } else {
+                    if (preg_match("/\.(" . $allowFiles . ")$/i", $file)) {
+                        $files [] = array(
+                            'url' => substr($path2, strlen($_SERVER ['DOCUMENT_ROOT'])),
+                            'mtime' => filemtime($path2)
+                        );
+                    }
+                }
+            }
+        }
+        return $files;
+    }
+
+    /**
+     * ×¥È¡Ô¶ï¿½ï¿½Í¼Æ¬
+     *
+     * @return string
+     */
+    protected function actionCrawler()
+    {
+        /* ï¿½Ï´ï¿½ï¿½ï¿½ï¿½ï¿½ */
+        $config = array(
+            "pathFormat" => $this->config ['catcherPathFormat'],
+            "maxSize" => $this->config ['catcherMaxSize'],
+            "allowFiles" => $this->config ['catcherAllowFiles'],
+            "oriName" => "remote.png"
+        );
+        $fieldName = $this->config ['catcherFieldName'];
+
+        /* ×¥È¡Ô¶ï¿½ï¿½Í¼Æ¬ */
+        $list = array();
+        if (isset ($_POST [$fieldName])) {
+            $source = $_POST [$fieldName];
+        } else {
+            $source = $_GET [$fieldName];
+        }
+        foreach ($source as $imgUrl) {
+            $item = new Uploader ($imgUrl, $config, "remote");
+            $info = $item->getFileInfo();
+            array_push($list, array(
+                "state" => $info ["state"],
+                "url" => $info ["url"],
+                "size" => $info ["size"],
+                "title" => htmlspecialchars($info ["title"]),
+                "original" => htmlspecialchars($info ["original"]),
+                "source" => htmlspecialchars($imgUrl)
+            ));
+        }
+
+        /* ï¿½ï¿½ï¿½ï¿½×¥È¡ï¿½ï¿½ï¿½ï¿½ */
+        return json_encode(array(
+            'state' => count($list) ? 'SUCCESS' : 'ERROR',
+            'list' => $list
+        ));
+    }
 }
